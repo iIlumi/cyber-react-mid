b9b862524b3a73aea7cf9a6f43b44822efae5fc9
danh tran <tldanh1993@gmail.com>
2022-01-24 14:58:02 +0700

    Pattern validate get error msg, Axios - POST

--- src/pages/ToDoList/ToDoList.js
+++ src/pages/ToDoList/ToDoList.js
@@ -8,6 +8,12 @@ import Axios from 'axios';
 export default class ToDoList extends Component {
   state = {
     taskList: [],
+    values: {
+      taskName: '',
+    },
+    errors: {
+      taskName: '',
+    },
   };
 
   // http://svcy.myclass.vn/swagger
@@ -35,54 +41,57 @@ export default class ToDoList extends Component {
       console.log(err.response.data);
     });
   };
-  /**
- 
   componentDidMount = () => {
     this.getTaskList();
   };
 
-  renderTaskToDo = () => {
-    return this.state.taskList
-      .filter((item) => !item.status)
-      .map((item, index) => {
-        return (
-          <li key={index}>
-            <span>{item.taskName}</span>
-            <div className="buttons">
-              <button className="remove">
-                <i className="fa fa-trash-alt" />
-              </button>
-              <button className="complete">
-                <i className="far fa-check-circle" />
-                <i className="fas fa-check-circle" />
-              </button>
-            </div>
-          </li>
-        );
-      });
+  // Pattern validate get error chung
+  handleChange = (e) => {
+    const { value, name } = e.target;
+    // console.log(value, name);
+
+    const newValues = { ...this.state.values, [name]: value };
+
+    const newErrors = { ...this.state.errors };
+
+    const regexString = /^[a-z A-Z]+$/;
+
+    if (!regexString.test(value) || value.trim() === '') {
+      newErrors[name] = name + ' invalid !';
+    } else {
+      newErrors[name] = '';
+    }
+
+    this.setState({
+      ...this.state,
+      values: newValues,
+      errors: newErrors,
+    });
   };
-  renderTaskToDoDone = () => {
-    return this.state.taskList
-      .filter((item) => item.status)
-      .map((item, index) => {
-        return (
-          <li key={index}>
-            <span>{item.taskName}</span>
-            <div className="buttons">
-              <button className="remove">
-                <i className="fa fa-trash-alt" />
-              </button>
-              <button className="complete">
-                <i className="far fa-check-circle" />
-                <i className="fas fa-check-circle" />
-              </button>
-            </div>
-          </li>
-        );
-      });
+
+  //   Bug addTask ảnh hưởng vào tất cả button đang có trong form
+  addTask = (e) => {
+    e.preventDefault(); //Dừng sự kiện submit form
+    console.log(this.state.values.taskName);
+    console.log('e.target:', e.target);
+    let promise = Axios({
+      url: 'http://svcy.myclass.vn/api/ToDoList/AddTask',
+      method: 'POST',
+      // Cấu trúc data trong POST phải xem API 
+      data: { taskName: this.state.values.taskName },
+    });
+
+    //Xử lý thành công
+    promise.then((result) => {
+      // alert(result.data);
+      this.getTaskList();
+    });
+
+    //Xử lý thất bại
+    promise.catch((errors) => {
+      alert(errors.response.data);
+    });
   };
-   
- */
 
   renderTaskList = (taskStatus) => {
     return this.state.taskList
@@ -104,10 +113,10 @@ export default class ToDoList extends Component {
         );
       });
   };
+
   render() {
     return (
-      <div>
-        <button onClick={this.getTaskList}>Get task list</button>
+      <form onSubmit={this.addTask}>
         <div className="card">
           <Helmet>
             <link rel="stylesheet" href="/cssHelmet/Todolist.css" />
@@ -115,63 +124,43 @@ export default class ToDoList extends Component {
           <div className="card__header">
             <img src={require('./bg.png')} />
           </div>
-          {/* <h2>hello!</h2> */}
           <div className="card__body">
             <div className="card__content">
               <div className="card__title">
                 <h2>My Tasks</h2>
                 <p>September 9,2020</p>
               </div>
-              <div className="card__add">
-                <input
-                  id="newTask"
-                  type="text"
-                  placeholder="Enter an activity..."
-                />
-                <button id="addItem">
-                  <i className="fa fa-plus" />
-                </button>
+              <div className="form-group">
+                <div className="card__add">
+                  <input
+                    id="newTask"
+                    type="text"
+                    placeholder="Enter an activity..."
+                    name="taskName"
+                    onChange={this.handleChange}
+                  />
+                  <button id="addItem" onClick={this.addTask}>
+                    <i className="fa fa-plus" />
+                  </button>
+                </div>
+                <span className="text text-danger">
+                  {this.state.errors.taskName}
+                </span>
               </div>
-              <div className="card__todo">
+              <div className="card__todo form-group">
                 {/* Uncompleted tasks */}
                 <ul className="todo" id="todo">
                   {this.renderTaskList(false)}
-                  {/* {this.renderTaskToDo()} */}
-                  {/* <li>
-                  <span>Đi ngủ</span>
-                  <div className="buttons">
-                    <button className="remove">
-                      <i className="fa fa-trash-alt" />
-                    </button>
-                    <button className="complete">
-                      <i className="far fa-check-circle" />
-                      <i className="fas fa-check-circle" />
-                    </button>
-                  </div>
-                </li> */}
                 </ul>
                 {/* Completed tasks */}
                 <ul className="todo" id="completed">
                   {this.renderTaskList(true)}
-                  {/* {this.renderTaskToDoDone()} */}
-                  {/* <li>
-                  <span>Ăn sáng</span>
-                  <div className="buttons">
-                    <button className="remove">
-                      <i className="fa fa-trash-alt" />
-                    </button>
-                    <button className="complete">
-                      <i className="far fa-check-circle" />
-                      <i className="fas fa-check-circle" />
-                    </button>
-                  </div>
-                </li> */}
                 </ul>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     );
   }
 }
