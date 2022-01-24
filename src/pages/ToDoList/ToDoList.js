27234d45b6e0e844cacb279b5b156e63eea2fd8d
danh tran <tldanh1993@gmail.com>
2022-01-24 13:56:34 +0700

    require for html img, axios GET - render manual

--- src/pages/ToDoList/ToDoList.js
+++ src/pages/ToDoList/ToDoList.js
@@ -1,35 +1,143 @@
 import React, { Component } from 'react';
-import style from './Todolist.css';
+// import style from './Todolist.css';
+// Cách dùng style vẫn ăn hết ?!
 // import './Todolist.css';
+import { Helmet } from 'react-helmet';
+import Axios from 'axios';
 
 export default class ToDoList extends Component {
-  render() {
-    return (
-      <div className="card">
-        <div className="card__header">
-          <img src="./img/bg.png" />
-        </div>
-        {/* <h2>hello!</h2> */}
-        <div className="card__body">
-          <div className="card__content">
-            <div className="card__title">
-              <h2>My Tasks</h2>
-              <p>September 9,2020</p>
-            </div>
-            <div className="card__add">
-              <input
-                id="newTask"
-                type="text"
-                placeholder="Enter an activity..."
-              />
-              <button id="addItem">
-                <i className="fa fa-plus" />
+  state = {
+    taskList: [],
+  };
+
+  // http://svcy.myclass.vn/swagger
+
+  getTaskList = () => {
+    let promise = Axios({
+      url: 'http://svcy.myclass.vn/api/ToDoList/GetAllTask',
+      method: 'GET',
+    });
+
+    promise.then((result) => {
+      console.log(result.data);
+      //Nếu gọi api lấy về kết quả thành công
+      //=> set lại state của component
+      this.setState({
+        taskList: result.data,
+      });
+
+      console.log('thành công');
+    });
+
+    promise.catch((err) => {
+      console.log('thất bại');
+
+      console.log(err.response.data);
+    });
+  };
+  /**
+ 
+  componentDidMount = () => {
+    this.getTaskList();
+  };
+
+  renderTaskToDo = () => {
+    return this.state.taskList
+      .filter((item) => !item.status)
+      .map((item, index) => {
+        return (
+          <li key={index}>
+            <span>{item.taskName}</span>
+            <div className="buttons">
+              <button className="remove">
+                <i className="fa fa-trash-alt" />
+              </button>
+              <button className="complete">
+                <i className="far fa-check-circle" />
+                <i className="fas fa-check-circle" />
               </button>
             </div>
-            <div className="card__todo">
-              {/* Uncompleted tasks */}
-              <ul className="todo" id="todo">
-                <li>
+          </li>
+        );
+      });
+  };
+  renderTaskToDoDone = () => {
+    return this.state.taskList
+      .filter((item) => item.status)
+      .map((item, index) => {
+        return (
+          <li key={index}>
+            <span>{item.taskName}</span>
+            <div className="buttons">
+              <button className="remove">
+                <i className="fa fa-trash-alt" />
+              </button>
+              <button className="complete">
+                <i className="far fa-check-circle" />
+                <i className="fas fa-check-circle" />
+              </button>
+            </div>
+          </li>
+        );
+      });
+  };
+   
+ */
+
+  renderTaskList = (taskStatus) => {
+    return this.state.taskList
+      .filter((item) => item.status === taskStatus)
+      .map((item, index) => {
+        return (
+          <li key={index}>
+            <span>{item.taskName}</span>
+            <div className="buttons">
+              <button className="remove">
+                <i className="fa fa-trash-alt" />
+              </button>
+              <button className="complete">
+                <i className="far fa-check-circle" />
+                <i className="fas fa-check-circle" />
+              </button>
+            </div>
+          </li>
+        );
+      });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.getTaskList}>Get task list</button>
+        <div className="card">
+          <Helmet>
+            <link rel="stylesheet" href="/cssHelmet/Todolist.css" />
+          </Helmet>
+          <div className="card__header">
+            <img src={require('./bg.png')} />
+          </div>
+          {/* <h2>hello!</h2> */}
+          <div className="card__body">
+            <div className="card__content">
+              <div className="card__title">
+                <h2>My Tasks</h2>
+                <p>September 9,2020</p>
+              </div>
+              <div className="card__add">
+                <input
+                  id="newTask"
+                  type="text"
+                  placeholder="Enter an activity..."
+                />
+                <button id="addItem">
+                  <i className="fa fa-plus" />
+                </button>
+              </div>
+              <div className="card__todo">
+                {/* Uncompleted tasks */}
+                <ul className="todo" id="todo">
+                  {this.renderTaskList(false)}
+                  {/* {this.renderTaskToDo()} */}
+                  {/* <li>
                   <span>Đi ngủ</span>
                   <div className="buttons">
                     <button className="remove">
@@ -40,11 +148,13 @@ export default class ToDoList extends Component {
                       <i className="fas fa-check-circle" />
                     </button>
                   </div>
-                </li>
-              </ul>
-              {/* Completed tasks */}
-              <ul className="todo" id="completed">
-                <li>
+                </li> */}
+                </ul>
+                {/* Completed tasks */}
+                <ul className="todo" id="completed">
+                  {this.renderTaskList(true)}
+                  {/* {this.renderTaskToDoDone()} */}
+                  {/* <li>
                   <span>Ăn sáng</span>
                   <div className="buttons">
                     <button className="remove">
@@ -55,8 +165,9 @@ export default class ToDoList extends Component {
                       <i className="fas fa-check-circle" />
                     </button>
                   </div>
-                </li>
-              </ul>
+                </li> */}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
