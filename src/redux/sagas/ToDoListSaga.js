bfde022f360b3a2079411dbc7df41b0a4471541b
danh tran <tldanh1993@gmail.com>
2022-01-31 17:45:15 +0700

    refactor getTask w constant and try-catch, cmt log

--- src/redux/sagas/ToDoListSaga.js
+++ src/redux/sagas/ToDoListSaga.js
@@ -1,6 +1,8 @@
 // import Axios from 'axios';
 import { call, delay, put, take, takeLatest } from 'redux-saga/effects';
 import { toDoListService } from '../../services/ToDoListService';
+import { STATUS_CODE } from '../../util/constants/settingSystem';
+import { DISPLAY_LOADING, HIDE_LOADING } from '../constants/LoadingConst';
 import { GET_TASK_API } from '../constants/ToDoListConst';
 
 // const url = 'http://svcy.myclass.vn/api/ToDoList/';
@@ -16,6 +18,11 @@ import { GET_TASK_API } from '../constants/ToDoListConst';
 
 // Cheatsheet saga blocking - non-blocking
 // https://redux-saga.js.org/docs/api#blocking--non-blocking
+
+/*
+    31/01/2021 Danh viết chức năng getTask
+    Action saga lấy danh sách task từ api 
+*/
 
 export function* getTaskApiWithTake() {
   while (true) {
@@ -45,38 +52,50 @@ export function* getTaskApiWithTake() {
 function* getTaskApiAction(action) {
   //put giống dispatch action
   yield put({
-    type: 'DISPLAY_LOADING',
+    type: DISPLAY_LOADING,
   });
 
-  yield delay(1000);
+  try {
+    // Tại sao phải yield call mà ko gọi bình thường
+    const { data, status } = yield call(
+      // nên tách các axios ra mục service chung
+      // () => {
+      // return Axios({
+      //   url: url + 'GetAllTask',
+      //   method: 'GET',
+      // });
+      // }
+      // Chú ý là gọi instance từ class chứ ko phải là ToDoService
+      // (toDo viết thường)
+      // Call nhận callback nên ko có ()
+      toDoListService.getTaskApi
+    );
+    yield delay(1000);
 
-  console.log('action getTaskApiAction:', action);
+    if (status !== STATUS_CODE.SUCCESS) {
+      console.log('STATUS_CODE error');
+    }
 
-  // Tại sao phải yield call mà ko gọi bình thường
-  const { data, status } = yield call(
-    // nên tách các axios ra mục service chung
-    // () => {
-    // return Axios({
-    //   url: url + 'GetAllTask',
-    //   method: 'GET',
-    // });
-    // }
-    // Chú ý là gọi instance từ class chứ ko phải là ToDoService
-    // (toDo viết thường)
-    // Call nhận callback nên ko có ()
-    toDoListService.getTaskApi
-  );
-  //Sau khi lấy giá trị thành công dùng put (giống dispatch bên thunk)
-  yield put({
-    type: GET_TASK_API,
-    taskList: data,
-  });
+    console.log('action getTaskApiAction:', action);
+    //Sau khi lấy giá trị thành công dùng put (giống dispatch bên thunk)
+    yield put({
+      type: GET_TASK_API,
+      taskList: data,
+    });
 
-  yield put({
-    type: 'HIDE_LOADING',
-  });
+    yield put({
+      type: HIDE_LOADING,
+    });
+  } catch (error) {
+    console.log('err');
+  }
 }
 
 export function* theoDoiActionGetTaskApi() {
   yield takeLatest('getTaskApiAction', getTaskApiAction);
 }
+
+/*
+    31/01/2021 Danh viết chức năng getTask
+    Action saga lấy danh sách task từ api 
+*/
