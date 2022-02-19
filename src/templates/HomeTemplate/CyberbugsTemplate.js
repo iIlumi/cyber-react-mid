117d2eeef05cd4b4c88db2558ce13b1a78bc709e
danh tran <tldanh1993@gmail.com>
2022-02-19 10:30:03 +0700

    > Init jira template resource

---
+++ src/templates/HomeTemplate/CyberbugsTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Home/Header/Header';
+
+export const CyberbygsTemplate = (props) => {
+  const { ele: Component, ...restParam } = props;
+
+  return (
+    <>
+
+    </>
+  );
+};
